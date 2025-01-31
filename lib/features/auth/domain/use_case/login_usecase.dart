c22d41465a8c7219b7ccd3c20b870fa0246caa97
Suman <sumaka37@gmail.com>
2025-01-31 17:52:06 +0545

    change in register usecase

--- lib/features/auth/domain/use_case/login_usecase.dart
+++ lib/features/auth/domain/use_case/login_usecase.dart
@@ -1,35 +1,46 @@
-import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
-import 'package:sprint4_fix/app/use_case/use_case.dart';
-import 'package:sprint4_fix/core/error/failure.dart';
-import 'package:sprint4_fix/features/auth/domain/repository/auth_repository.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:equatable/equatable.dart';
+// import 'package:sprint4_fix/app/use_case/use_case.dart';
+// import 'package:sprint4_fix/core/error/failure.dart';
+// import 'package:sprint4_fix/features/auth/domain/repository/auth_repository.dart';
 
-class LoginParams extends Equatable {
-  final String username;
-  final String password;
+// class LoginParams extends Equatable {
+//   final String username;
+//   final String password;
 
-  const LoginParams({
-    required this.username,
-    required this.password,
-  });
+//   const LoginParams({
+//     required this.username,
+//     required this.password,
+//   });
 
-  // Initial Constructor
-  const LoginParams.initial()
-      : username = '',
-        password = '';
+//   // Initial Constructor
+//   const LoginParams.initial()
+//       : username = '',
+//         password = '';
 
-  @override
-  List<Object> get props => [username, password];
-}
+//   @override
+//   List<Object> get props => [username, password];
+// }
 
-class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
-  final IAuthRepository repository;
+// class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
+//   final IAuthRepository repository;
+//   final TokenSharedPrefs tokenSharedPrefs;
 
-  LoginUseCase(this.repository);
+//   LoginUseCase(this.repository, this.tokenSharedPrefs);
 
-  @override
-  Future<Either<Failure, String>> call(LoginParams params) {
-    // IF api then store token in shared preferences
-    return repository.logincustomer(params.username, params.password);
-  }
-}
+//   @override
+//   Future<Either<Failure, String>> call(LoginParams params) {
+//     // IF api then store token in shared preferences
+//     return repository
+//         .logincustomer(params.username, params.password)
+//         .then((value) {
+//       return value.fold((faliure) => Left(faliure), (token) {
+//         tokenSharedPrefs.saveToken(token);
+//         tokenSharedPrefs.getToken().then((value) {
+//           print(value);
+//         });
+//         return Right(token);
+//       });
+//     });
+//   }
+// }
