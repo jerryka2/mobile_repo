060cfd10b9631b9b09050b300a328fcc6c57be48
Suman <sumaka37@gmail.com>
2025-01-31 19:53:50 +0545

    api_service

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -1,19 +1,24 @@
-// class ApiService {
-//   final Dio _dio;
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:sprint4_fix/app/constants/api_endpoint.dart';
+import 'package:sprint4_fix/core/network/dio_error_incterceptor.dart';
 
-//   Dio get dio => _dio;
+class ApiService {
+  final Dio _dio;
 
-//   ApiService(this._dio) {
-//     _dio
-//       ..options.baseUrl = ApiEndpoints.baseUrl
-//       ..options.connectTimeout = ApiEndpoints.connectionTimeout
-//       ..options.receiveTimeout = ApiEndpoints.receiveTimeout
-//       ..interceptors.add(DioErrorInterceptor())
-//       ..interceptors.add(PrettyDioLogger(
-//           requestHeader: true, requestBody: true, responseHeader: false))
-//       ..options.headers = {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json',
-//       };
-//   }
-// }
+  Dio get dio => _dio;
+
+  ApiService(this._dio) {
+    _dio
+      ..options.baseUrl = ApiEndpoints.baseUrl
+      ..options.connectTimeout = ApiEndpoints.connectionTimeout
+      ..options.receiveTimeout = ApiEndpoints.receiveTimeout
+      ..interceptors.add(DioErrorInterceptor())
+      ..interceptors.add(PrettyDioLogger(
+          requestHeader: true, requestBody: true, responseHeader: false))
+      ..options.headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      };
+  }
+}
