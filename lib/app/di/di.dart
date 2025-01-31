449e5a5a97892752a7d507f99730d420db935059
Suman <sumaka37@gmail.com>
2025-01-31 19:53:35 +0545

    di.dart

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,9 +1,16 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sprint4_fix/app/shared_prefs/token_shared_prefs.dart';
+import 'package:sprint4_fix/core/network/api_service.dart';
 import 'package:sprint4_fix/core/network/hive_service.dart';
 import 'package:sprint4_fix/features/auth/data/data_source/auth_local_datasource/auth_local_datasource.dart';
+import 'package:sprint4_fix/features/auth/data/data_source/auth_remote_datasource.dart/auth_remote_datasource.dart';
 import 'package:sprint4_fix/features/auth/data/repository/auth_local_repository.dart';
+import 'package:sprint4_fix/features/auth/data/repository/auth_remote_repository/auth_remote_repository.dart';
 import 'package:sprint4_fix/features/auth/domain/use_case/login_usecase.dart';
 import 'package:sprint4_fix/features/auth/domain/use_case/register_user_usecase.dart';
+import 'package:sprint4_fix/features/auth/domain/use_case/upload_image_usecase.dart';
 import 'package:sprint4_fix/features/auth/presentation/view_model/login/login_bloc.dart';
 import 'package:sprint4_fix/features/auth/presentation/view_model/signup/register_bloc.dart';
 import 'package:sprint4_fix/features/home/view_model/home_cubit.dart';
@@ -13,12 +20,23 @@ final getIt = GetIt.instance;
 Future<void> initDependencies() async {
   // First initialize hive service
   await _initHiveService();
+  await _initApiService();
+  await _initSharedPreferences();
 
   await _initHomeDependencies();
   await _initRegisterDependencies();
   await _initLoginDependencies();
+}
 
-  // await _initSplashScreenDependencies();
+Future<void> _initSharedPreferences() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+}
+
+_initApiService() async {
+  getIt.registerLazySingleton<Dio>(
+    () => ApiService(Dio()).dio,
+  );
 }
 
 _initHiveService() {
@@ -31,9 +49,22 @@ _initRegisterDependencies() {
     () => AuthLocalDataSource(getIt<HiveService>()),
   );
 
+  getIt.registerLazySingleton<AuthRemoteDatasource>(
+    () => AuthRemoteDatasource(getIt<Dio>()),
+  );
+
   // init local repository
   getIt.registerLazySingleton(
     () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
+  );
+
+  getIt.registerLazySingleton(
+    () => AuthRemoteRepository(getIt<AuthRemoteDatasource>()),
+  );
+
+  // Register UploadImageUseCase (Missing Previously)
+  getIt.registerLazySingleton<UploadImageUsecase>(
+    () => UploadImageUsecase(getIt<AuthRemoteRepository>()),
   );
 
   // register use usecase
@@ -46,6 +77,7 @@ _initRegisterDependencies() {
   getIt.registerFactory<RegisterBloc>(
     () => RegisterBloc(
       registerUseCase: getIt(),
+      uploadImageUseCase: getIt(), // Now it will not throw an error
     ),
   );
 }
@@ -57,9 +89,14 @@ _initHomeDependencies() async {
 }
 
 _initLoginDependencies() async {
+  getIt.registerLazySingleton<TokenSharedPrefs>(
+    () => TokenSharedPrefs(getIt<SharedPreferences>()),
+  );
+
   getIt.registerLazySingleton<LoginUseCase>(
     () => LoginUseCase(
       getIt<AuthLocalRepository>(),
+      getIt<TokenSharedPrefs>(),
     ),
   );
 
@@ -71,9 +108,3 @@ _initLoginDependencies() async {
     ),
   );
 }
-
-// _initSplashScreenDependencies() async {
-//   getIt.registerFactory<SplashCubit>(
-//     () => SplashCubit(getIt<LoginBloc>()),
-//   );
-// }
