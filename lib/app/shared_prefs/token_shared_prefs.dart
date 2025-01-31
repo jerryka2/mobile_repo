03c70e080148728aed6f7645b6f1ab14cde5e8db
Suman <sumaka37@gmail.com>
2025-01-31 19:53:42 +0545

    token

--- lib/app/shared_prefs/token_shared_prefs.dart
+++ lib/app/shared_prefs/token_shared_prefs.dart
@@ -1,23 +1,27 @@
-// class TokenSharedPrefs {
-//   final SharedPreferences _sharedPreferences;
+import 'package:dartz/dartz.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sprint4_fix/core/error/failure.dart';
 
-//   TokenSharedPrefs(this._sharedPreferences);
+class TokenSharedPrefs {
+  final SharedPreferences _sharedPreferences;
 
-//   Future<Either<Failure, String>> saveToken(String token) async {
-//     try {
-//       await _sharedPreferences.setString('token', token);
-//       return Right(token);
-//     } catch (e) {
-//       return Left(SharedPrefsFailure(message: e.toString()));
-//     }
-//   }
+  TokenSharedPrefs(this._sharedPreferences);
 
-//   Future<Either<Failure, String>> getToken() async {
-//     try {
-//       final token = _sharedPreferences.getString('token');
-//       return Right(token ?? '');
-//     } catch (e) {
-//       return Left(SharedPrefsFailure(message: e.toString()));
-//     }
-//   }
-// }
+  Future<Either<Failure, String>> saveToken(String token) async {
+    try {
+      await _sharedPreferences.setString('token', token);
+      return Right(token);
+    } catch (e) {
+      return Left(SharedPrefsFailure(message: e.toString()));
+    }
+  }
+
+  Future<Either<Failure, String>> getToken() async {
+    try {
+      final token = _sharedPreferences.getString('token');
+      return Right(token ?? '');
+    } catch (e) {
+      return Left(SharedPrefsFailure(message: e.toString()));
+    }
+  }
+}
