5184b6c653668d92f8e44279e17544f26192e0c6
Suman <sumaka37@gmail.com>
2025-01-31 19:54:24 +0545

    auth_remote

--- lib/features/auth/data/repository/auth_remote_repository/auth_remote_repository.dart
+++ lib/features/auth/data/repository/auth_remote_repository/auth_remote_repository.dart
@@ -1,43 +1,51 @@
-// class AuthRemoteRepository implements IAuthRepository {
-//   final AuthRemoteDatasource _authRemoteDatasource;
+import 'dart:io';
 
-//   AuthRemoteRepository(this._authRemoteDatasource);
+import 'package:dartz/dartz.dart';
+import 'package:sprint4_fix/core/error/failure.dart';
+import 'package:sprint4_fix/features/auth/data/data_source/auth_remote_datasource.dart/auth_remote_datasource.dart';
+import 'package:sprint4_fix/features/auth/domain/entity/auth_entity.dart';
+import 'package:sprint4_fix/features/auth/domain/repository/auth_repository.dart';
 
-//   @override
-//   Future<Either<Failure, AuthEntity>> getCurrentUser() {
-//     // TODO: implement getCurrentUser
-//     throw UnimplementedError();
-//   }
+class AuthRemoteRepository implements IAuthRepository {
+  final AuthRemoteDatasource _authRemoteDatasource;
 
-//   @override
-//   Future<Either<Failure, String>> logincustomer(
-//       String username, String password) async {
-//     try {
-//       final token =
-//           await _authRemoteDatasource.logincustomer(username, password);
-//       return Right(token);
-//     } catch (e) {
-//       return Left(ApiFailure(message: e.toString()));
-//     }
-//   }
+  AuthRemoteRepository(this._authRemoteDatasource);
 
-//   @override
-//   Future<Either<Failure, void>> registercustomer(AuthEntity customer) async {
-//     try {
-//       await _authRemoteDatasource.registercustomer(customer);
-//       return const Right(null);
-//     } catch (e) {
-//       return Left(ApiFailure(message: e.toString()));
-//     }
-//   }
+  @override
+  Future<Either<Failure, AuthEntity>> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
 
-//   @override
-//   Future<Either<Failure, String>> uploadProfilePicture(File file) async {
-//     try {
-//       final imageName = await _authRemoteDatasource.uploadProfilePicture(file);
-//       return Right(imageName);
-//     } catch (e) {
-//       return Left(ApiFailure(message: e.toString()));
-//     }
-//   }
-// }
+  @override
+  Future<Either<Failure, String>> logincustomer(
+      String username, String password) async {
+    try {
+      final token =
+          await _authRemoteDatasource.logincustomer(username, password);
+      return Right(token);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> registercustomer(AuthEntity customer) async {
+    try {
+      await _authRemoteDatasource.registercustomer(customer);
+      return const Right(null);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, String>> uploadProfilePicture(File file) async {
+    try {
+      final imageName = await _authRemoteDatasource.uploadProfilePicture(file);
+      return Right(imageName);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
+  }
+}
